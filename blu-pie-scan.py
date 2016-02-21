bebbdfaed16c3ac8744802b66eb28c88ba61bac6
NukeAlchemist <NukeAlchemist@users.noreply.github.com>
2016-02-20 18:05:59 -0600

    initial code

--- blu-pie-scan.py
+++ blu-pie-scan.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
-from subprocess import call
+import sys
+import subprocess
+from time import sleep
+
+# Global variables until we have a config file
+fmin = 151000000
+fmax = 156500000
+
+# Parameter formatting
+frange = str(fmin) + ":" + str(fmax)
+frange = "-f " + frange
 
 
 # Let's run this bitch!
@@ -8,8 +18,16 @@ if __name__ == '__main__':
 
 	try:
 
-		print "TEMP"
+		print "Starting up..."
+		
+		# Insert baseline file generation subroutine here once main script is completed
+
+		rtlscan = subprocess.Popen(['/usr/local/bin/rtl_power_fftw', '-b 512', frange, '-c'], stdout=subprocess.PIPE)
+
+		lines_iterator = iter(rtlscan.stdout.readline, b"")
+		for line in lines_iterator:
+			print line
 
 	except (KeyboardInterrupt, SystemExit): # Press ctrl-c
 
-		call(["killall", "-9", "rtl_power_fftw"])
+		rtlscan.kill()
