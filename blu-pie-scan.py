315d14e0483a8a74fd806876d34f888b961f0f6c
NukeAlchemist <NukeAlchemist@users.noreply.github.com>
2016-02-20 21:30:54 -0600

    workin' on it...

--- blu-pie-scan.py
+++ blu-pie-scan.py
@@ -1,32 +1,48 @@
 #!/usr/bin/env python
 
 import sys
-import subprocess
+import subprocess as sp
+import shlex
 from time import sleep
 
 # Global variables until we have a config file
 fmin = 151000000
 fmax = 156500000
+alarmthresh = -10
+powerfftw_path = "/usr/local/bin/rtl_power_fftw"
+fftbins = 512
+otherargs = "-c"
 
 # Parameter formatting
 frange = str(fmin) + ":" + str(fmax)
 frange = "-f " + frange
-
+fftbins = "-b " + str(fftbins)
 
 # Let's run this bitch!
 if __name__ == '__main__':
+	
+	floats = []
 
 	try:
-
 		print "Starting up..."
 		
 		# Insert baseline file generation subroutine here once main script is completed
 
-		rtlscan = subprocess.Popen(['/usr/local/bin/rtl_power_fftw', '-b 512', frange, '-c'], stdout=subprocess.PIPE)
+		rtlscan = sp.Popen([powerfftw_path, frange, fftbins, otherargs], stdout=sp.PIPE)
 
 		lines_iterator = iter(rtlscan.stdout.readline, b"")
 		for line in lines_iterator:
-			print line
+
+			floats = []
+			if '#' in line:
+				floats = [0,-99]
+			else:
+				floats = map(float, line.split())
+
+
+			if len(floats) > 1:
+				if floats[1] >= alarmthresh:
+					print floats
 
 	except (KeyboardInterrupt, SystemExit): # Press ctrl-c
 
