2c1ff8d7dfa964be7ebdd1e18dd9fa7304b8a0fd
NukeAlchemist <NukeAlchemist@users.noreply.github.com>
2016-02-21 01:21:15 -0600

    Core code functional.

--- blu-pie-scan.py
+++ blu-pie-scan.py
@@ -3,25 +3,28 @@
 import sys
 import subprocess as sp
 import time
+from os import devnull
 
 # Global variables until we have a config file
 fmin = 151000000
 fmax = 156500000
-alarmthresh = -5
+alarmthresh = 4
 powerfftw_path = "/usr/local/bin/rtl_power_fftw"
 fftbins = 512
 otherargs = "-c"
-block_scan_time = 1.25
+block_scan_time = 0.75
 
 # Parameter formatting
 frange = str(fmin) + ":" + str(fmax)
 frange = "-f " + frange
 fftbins = "-b " + str(fftbins)
 bstime = "-t " + str(block_scan_time)
+dvnll = open(devnull, 'wb')
 
 # Let's run this bitch!
 if __name__ == '__main__':
-	
+
+	# Housekeeping:
 	floats = []
 
 	try:
@@ -29,7 +32,7 @@ if __name__ == '__main__':
 		
 		# Insert baseline file generation subroutine here once main script is completed
 
-		rtlscan = sp.Popen([powerfftw_path, frange, fftbins, otherargs, bstime], stdout=sp.PIPE, stderr=sp.PIPE)
+		rtlscan = sp.Popen([powerfftw_path, frange, fftbins, otherargs, bstime], stdout=sp.PIPE, stderr=dvnll, shell=False)
 
 		lines_iterator = iter(rtlscan.stdout.readline, b"")
 		for line in lines_iterator:
@@ -39,9 +42,9 @@ if __name__ == '__main__':
 			else:
 				floats = map(float, line.split())
 
-			if int(floats[1]) >= alarmthresh:
+			if floats[1] >= alarmthresh:
 
-				freq_temp = round(floats[0]/1000000, 3)
+				freq_temp = round(floats[0] /1000000, 3)
 				print "At " + time.strftime("%H:%M:%S") + ", a " + str(round(floats[1], 1)) + " dB/Hz signal was detected at " + str(freq_temp) + " MHz."
 
 	except (KeyboardInterrupt, SystemExit): # Press ctrl-c
